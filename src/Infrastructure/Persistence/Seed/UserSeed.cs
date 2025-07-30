184583ec7122a1884fab67d154b1bd628d338029
taner04 <126077254+taner04@users.noreply.github.com>
2025-07-30 16:29:24 +0200

    Seed for User with JWT

--- src/Infrastructure/Persistence/Seed/UserSeed.cs
+++ src/Infrastructure/Persistence/Seed/UserSeed.cs
@@ -6,10 +6,16 @@ namespace Infrastructure.Persistence.Seed
     {
         public static void Seed(DbContext dbContext)
         {
-            var dummyUser = new User("John", "Doe", "doe@mail.com", "John123!");
+            var user = new User("John", "Doe", "doe@mail.com", "John123!")
+            {
+                Jwt = new Jwt(
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QG1haWwuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoidGVzdEBtYWlsLmNvbSIsIm5iZiI6MTc1Mzg4NTQyOSwiZXhwIjoxNzUzODkyNjI5LCJpc3MiOiJodHRwczovL3d3dy5lU2hvcC1hcGkuY29tIiwiYXVkIjoiZVNob3AtQXBpIn0.LJf4HNEJE8KLwnfBaVcO-MbJI_vXNAg_ZDdfkIwUoZ4",
+                    DateTime.UtcNow,
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QG1haWwuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoidGVzdEBtYWlsLmNvbSIsIm5iZiI6MTc1Mzg4NTQyOSwiZXhwIjoxNzU0NDkwMjI5LCJpc3MiOiJodHRwczovL3d3dy5lU2hvcC1hcGkuY29tIiwiYXVkIjoiZVNob3AtQXBpIn0.6byhPpfHXqbF2lDjOUgWoQ8v8O45Bnbh_R8W0pib2oA",
+                    DateTime.UtcNow)
+            };
 
-            dbContext.Set<User>().RemoveRange(dbContext.Set<User>().ToList());
-            dbContext.Set<User>().Add(dummyUser);
+            dbContext.Set<User>().Add(user);
         }
     }
 }
